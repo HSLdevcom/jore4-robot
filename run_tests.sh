38c20fbaab9a5d05462c9e165e5f48568a861edd
Joonas Peuralinna <joonas.peuralinna@valagroup.com>
2021-04-01 08:31:46 +0300

    trying if creating results folder before tests helps with permission problem

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -t robot-browser .
  docker run --rm \
-    --volume "$PWD/results":/tests/results \
+    --volume "$PWD/results":/tests/Results \
      robot-browser \
-       bash -c "robot -v ENV:$1 -v BROWSER:$2 --outputdir /tests/results /tests"
+       bash -c "robot -v ENV:$1 -v BROWSER:$2 --outputdir /tests/Results /tests"
