b1290ca57cbd926caf42e550e5bc4024543a06cc
Joonas Peuralinna <joonas.peuralinna@valagroup.com>
2021-04-01 08:38:40 +0300

    trying different folder layout

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -t robot-browser .
  docker run --rm \
-    --volume "$PWD/results":/tests/Results \
+    --volume "$PWD/tests/Results":/tests/Results \
      robot-browser \
        bash -c "robot -v ENV:$1 -v BROWSER:$2 --outputdir /tests/Results /tests"
