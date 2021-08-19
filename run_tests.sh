11d1fbdc93dc715383a0624fcf72edfbeeb904f1
Joonas Peuralinna <joonas.peuralinna@valagroup.com>
2021-08-19 08:38:48 +0300

    dummy push

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 docker build -t robot-browser .
  docker run --rm \
-  --network="host" \
     --volume "$PWD/tests":/tests\
      robot-browser \
        bash -c "robot -v ENV:$1 -v BROWSER:$2 --outputdir /tests/output /tests"
