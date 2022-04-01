40c8a7590af68c5be78f7726ab525da674b9e3b7
Tamas Pallos <tamas.pallos@vincit.fi>
2022-04-01 09:48:43 +0300

    Added parameter to run_tests.sh for choosing which tags to include

--- run_tests.sh
+++ run_tests.sh
@@ -11,9 +11,13 @@ docker build --cache-from=hsldevcom/jore4-robot:latest -t robot-browser .
 # read the password and email values from the '.env' file
 export $(cat .env | xargs)
 
+TEST_ENVIRONMENT=$1
+TEST_BROWSER=$2
+INCLUDE_TAGS=${3:-*}
+
 # run the tests
 docker run --rm \
   --network="host" \
   --volume "$PWD/tests":/tests \
   robot-browser \
-  bash -c "robot -v ENV:$1 -v BROWSER:$2 -v PASSWORD:$E2E_USER_PASSWORD -v EMAIL:$E2E_USER_USERNAME --outputdir /tests/output /tests"
+  bash -c "robot -v ENV:$TEST_ENVIRONMENT -v BROWSER:$TEST_BROWSER -v PASSWORD:$E2E_USER_PASSWORD -v EMAIL:$E2E_USER_USERNAME --outputdir /tests/output --include \"$INCLUDE_TAGS\" /tests"
