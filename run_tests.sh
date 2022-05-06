f32d6f5a8b5aab2bd6cbd402cafcf4ef3e0fb2ea
Tamas Pallos <tamas.pallos@vincit.fi>
2022-05-06 09:55:24 +0300

    Remove unnecessary docker pull command
    The docker pull actually was just slowing the whole build&run process instead of speeding it up

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-# download previous robot test docker image for faster builds
-docker pull hsldevcom/jore4-robot:latest || echo "Previous image not found"
-
 # build robot test docker image, using cache from previous build
 docker build --cache-from=hsldevcom/jore4-robot:latest -t robot-browser .
 
